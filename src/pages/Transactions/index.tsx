231f1248a6f9ffeb27414faa15d7611dafc4ad1a
Alcino José Lincoln de Souza <lincoln_tec@hotmail.com>
2023-10-18 18:27:32 -0300

    Creation of the Summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Headers } from "../../components/Headers";
+import { Summary } from "../../components/Summary";
 
 export function Transactions() {
     return (
         <div>
             <Headers />
+            <Summary />
 
         </div>
     )
