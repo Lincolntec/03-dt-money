77146633519960a901e311916bdbd50bda05a81a
Alcino José Lincoln de Souza <lincoln_tec@hotmail.com>
2023-10-19 08:44:05 -0300

    Transaction table creation

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,44 @@
 import { Headers } from "../../components/Headers";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Headers />
             <Summary />
+            <TransactionsContainer >
+                <TransactionsTable>
+                    <tbody>
+
+                        <tr>
+                            <td width="45%">Desenvolvimento de site.</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+
+                            </td>
+
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="45%">Hamburger.</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    -R$ 59,00
+                                </PriceHighlight>
+
+                            </td>
+
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
 
         </div>
     )
